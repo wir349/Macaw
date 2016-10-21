a8958890013fc3360a6ce1db1eecab0a1d5ab568
Victor Sukochev <sukochevvv@gmail.com>
2016-10-21 22:49:59 +0700

    Tests fix

--- MacawTests/TestUtils.swift
+++ MacawTests/TestUtils.swift
@@ -11,7 +11,7 @@ class TestUtils {
 				let group = SVGParser.parse(text: svgString)
 				let referenceArray = TestUtils.prepareParametersList(Mirror(reflecting: referenceObject))
 				let parametersArray = TestUtils.prepareParametersList(Mirror(reflecting: group))
-				return referenceArray.elementsEqual(parametersArray, isEquivalent: { first, second in
+				return referenceArray.elementsEqual(parametersArray, by: { first, second in
 					return first.0 == second.0 && first.1 == second.1
 				})
 			}
@@ -22,7 +22,7 @@ class TestUtils {
 	class func prepareParametersList(_ mirror: Mirror) -> [(String, String)] {
 		var result: [(String, String)] = []
 		for (_, attribute) in mirror.children.enumerated() {
-			if let label = attribute.label , label == "_value" || label.characters.first != "_" {
+			if let label = attribute.label , (label == "_value" || label.characters.first != "_") && label != "contentsVar" {
 				result.append((label, String(describing: attribute.value)))
 				result.append(contentsOf: prepareParametersList(Mirror(reflecting: attribute.value)))
 			}
